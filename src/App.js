1f7c324dc8e604806f762d578b370ec1c21a8f22
kadelsafal <np03cs4a220513@heraldcollege.edu.np>
2024-04-16 22:54:53 +0545

    'updates'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+/**App.js */
 import React, { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, useHistory } from "react-router-dom";
 import Menu from './pages/Menu';
 import Story from './pages/Story';
 import Location from './pages/Location';
 import Tables from "./pages/Tables";
 import Footer from './Components/Footer';
-import LoginPopup from './Components/loginpop'; // Import the LoginPopup component
-import SignupPopup from './Components/signupop'; // Import the SignupPopup component
+import LoginPopup from './Components/loginpop';
+import SignupPopup from './Components/signupop';
+
 
 function App() {
   const [showLoginPopup, setShowLoginPopup] = useState(false);
   const [showSignupPopup, setShowSignupPopup] = useState(false);
+  const history = useHistory();
 
   const openLoginPopup = () => {
     setShowLoginPopup(true);
@@ -21,7 +24,8 @@ function App() {
 
   const closeLoginPopup = () => {
     setShowLoginPopup(false);
-  };
+    setShowSignupPopup(false);
+    };
 
   const openSignupPopup = () => {
     setShowSignupPopup(true);
@@ -29,24 +33,35 @@ function App() {
 
   const closeSignupPopup = () => {
     setShowSignupPopup(false);
-      // Close the login popup when signup popup is closed
-      setShowLoginPopup(false);
+    
+    setShowLoginPopup(false);
+    
   };
+  const handleLogin = () => {
+    // Define your login logic here
+    console.log("Login successful");
+    // You can perform any actions after successful login
+    
+  };
+
 
   return (
     <div className="App">
       <Router>
         <Navbar onOpenLoginPopup={openLoginPopup} />
         <div className='main-content'>
+         
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/Menu" exact component={Menu} />
             <Route path="/Story" exact component={Story} />
             <Route path="/Location" exact component={Location} />
             <Route path="/Tables" exact component={Tables} />
+            
           </Switch>
-          {showLoginPopup && <LoginPopup onClose={closeLoginPopup} onOpenSignupPopup={openSignupPopup} />}
-          {showSignupPopup && <SignupPopup onClose={closeSignupPopup} />}
+          {showLoginPopup && <LoginPopup onClose={closeLoginPopup} onOpenSignupPopup={openSignupPopup}  handleLogin={handleLogin}/>}
+          {showSignupPopup && <SignupPopup onClose={closeSignupPopup} onOpenLoginPopup={openLoginPopup} handleLogin={handleLogin}/>}
+
         </div>
         <Footer />
       </Router>
