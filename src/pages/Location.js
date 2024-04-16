b9d7b6a91275e999b90c50f9984cf5fa6ebdb35d
mokshadarana <np03cs4a220359@heraldcollege.edu.np>
2024-04-16 16:23:11 +0545

    Update to location page

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,16 +1,17 @@
-import React from 'react';
 import '../styles/location.css';
+import header_img from '../assets/table.webp';
+import location_map from '../assets/location_map.png'
+
 function Location() {
   return (
-    <div style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-    }}>
-      <h1>
-        GeeksforGeeks is a Computer Science portal for geeks.
-      </h1>
+    <div className='location-page'>
+      <div className='location-header'>
+        <img src={header_img} alt="food" className='food-image'/>
+      </div>
+      <h1>OUR LOCATION</h1>
+      <div className='location-map'>
+        <img src={location_map} alt="Location" className='location-image'/>
+      </div>
     </div>
   );
 }
