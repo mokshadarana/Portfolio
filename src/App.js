814e5c4baaee1ba4426a784964d7c9af46646309
mokshadarana <np03cs4a220359@heraldcollege.edu.np>
2024-06-11 12:46:06 +0545

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,77 +1,12 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
 import './App.css';
-import Navbar from './Components/Navbar';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Menu from './pages/Menu';
-import Story from './pages/Story';
-import Location from './pages/Location';
-import Tables from "./pages/Tables";
-import Footer from './Components/Footer';
-import LoginPopup from './Components/loginpop';
-import SignupPopup from './Components/signupop';
 
 function App() {
-  const [showLoginPopup, setShowLoginPopup] = useState(false);
-  const [showSignupPopup, setShowSignupPopup] = useState(false);
-
-  const openLoginPopup = () => {
-    setShowLoginPopup(true);
-  };
-
-  const closeLoginPopup = () => {
-    setShowLoginPopup(false);
-    setShowSignupPopup(false);
-  };
-
-  const openSignupPopup = () => {
-    setShowSignupPopup(true);
-  };
-
-  const closeSignupPopup = () => {
-    setShowSignupPopup(false);
-    setShowLoginPopup(false);
-  };
-
-  const handleLogin = () => {
-    console.log("Login successful");
-    // You can perform any actions after successful login
-  };
-
   return (
     <div className="App">
-      <Router>
-        <Navbar onOpenLoginPopup={openLoginPopup} />
-        <div className='main-content'>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/Menu" exact component={Menu} />
-            <Route path="/Story" exact component={Story} />
-            <Route path="/Location" exact component={Location} />
-            <Route path="/Tables" exact component={Tables} />
-          </Switch>
-          {showLoginPopup && <LoginPopup onClose={closeLoginPopup} onOpenSignupPopup={openSignupPopup} handleLogin={handleLogin} />}
-          {showSignupPopup && <SignupPopup onClose={closeSignupPopup} onOpenLoginPopup={openLoginPopup} handleLogin={handleLogin} />}
-        </div>
-        <Footer />
-      </Router>
+      <Home />
     </div>
   );
 }
-
-// Prop types validation
-Navbar.propTypes = {
-  onOpenLoginPopup: PropTypes.func.isRequired
-};
-
-LoginPopup.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  onOpenSignupPopup: PropTypes.func.isRequired
-};
-
-SignupPopup.propTypes = {
-  onClose: PropTypes.func.isRequired
-};
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,34 @@
-import '../styles/home.css'; // Import CSS file for styling
-import foodImage from '../assets/f2.jpg'; // Import image assets
-import momoImage from '../assets/momo.jpg';
-import pastaImage from '../assets/pasta.jpg';
-import tunaImage from '../assets/tuna.jpg';
-import f1Image from '../assets/f1.jpg';
+import "../styles/Home.css";
+import pic from '../assets/moks.jpg';
 
-const Homepage = () => {
-  return (
-    <main>
-      <section className="section-picture">
-        <div className="picture">
-          <img src={foodImage} alt="Food" />
-        </div>
-      </section>
-      <section className="section-about">
-        <div className="about-content">
-          <h2>About Us</h2>
-          <p style={{ fontFamily: 'Manrope', fontSize: '18px' }}>{"Introducing &apos;One Bite Foods&apos;  an innovative restaurant dine-in booking system designed to streamline your dining experience. Say goodbye to long waits and frustrating reservations. With One Bite Foods, diners can effortlessly book their tables in advance, ensuring a seamless dining experience from start to finish. Whether it&apos;s a cozy dinner for two or a celebratory gathering with friends, One Bite Foods ensures that every dining occasion is memorable, convenient, and enjoyable."}</p>
+const Home = () => {
+    return (
+        <main>
+            <div className="full">
+                <div className="header">
+                    <span className="text">Mokshada</span>
+                    <span className="text">Rana</span>
+                    <p className="intro-text">
+                        Hi, I&apos;m Mokshada Rana.<br />
+                        Step into my journey, where creativity meets technology.
+                        I&apos;m a passionate web developer dedicated to transforming ideas into dynamic digital experiences.
+                        With a flair for coding and an eye for design, I bring a unique blend of technical expertise and artistic vision to every project I undertake.
+                    </p>
+                </div>
 
-        </div>
-      </section>
-      <section className="section-top-sales">
-        <div className="pictures3">
-          <h3>Our Top Sales</h3>
-          <div className="picture-item">
-            <img src={momoImage} alt="Food" />
-            <p>MOMO</p>
-          </div>
-          <div className="picture-item">
-            <img src={pastaImage} alt="Food" />
-            <p>Pasta</p>
-          </div>
-          <div className="picture-item">
-            <img src={tunaImage} alt="Food" />
-            <p>Tuna</p>
-          </div>
-          <div className="picture-item">
-            <img src={f1Image} alt="Food" />
-            <p>Noodles</p>
-          </div>
-        </div>
-      </section>
-    </main>
-  );
+                <div className="content">
+                    <div className="about">
+                        <p>ABOUT ME</p>
+                    </div>
+                    <div className="image">
+                        <img src={pic} alt='image of Mokshada' />
+                    </div>
+                </div>
+
+
+            </div>
+        </main>
+    );
 }
 
-export default Homepage;
+export default Home;
